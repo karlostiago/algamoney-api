4f5557ea6626526dd85635f97a7a56db16d9cf38
Carlos Tiago <carlostiagodesousa@gmail.com>
2020-01-05 18:26:12 -0300

    criando primeira migracao com flyway

---
+++ src/main/resources/db/migration/V01__CRIAR_REGISTRAR_CATEGORIA.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE categoria (
+	codigo serial primary key not null,
+	nome varchar(255) not null
+)WITH (
+  OIDS=FALSE
+);
+ALTER TABLE public.categoria
+  OWNER TO postgres;
+
+INSERT INTO categoria (nome) VALUES ('LAZER');
+INSERT INTO categoria (nome) VALUES ('ALIMENTAÇÃO');
+INSERT INTO categoria (nome) VALUES ('SUPERMERCADO');
+INSERT INTO categoria (nome) VALUES ('FARMÁCIA');
+INSERT INTO categoria (nome) VALUES ('OUTROS');
