1da0e7117038ef84190fd153cf1a50a2b5fae1c5
Carlos Tiago <carlostiagodesousa@gmail.com>
2021-04-29 15:32:14 -0300

    ajustando profiles active

--- src/main/java/com/ctsousa/algamoney/api/config/property/AlgamoneyApiProperty.java
+++ src/main/java/com/ctsousa/algamoney/api/config/property/AlgamoneyApiProperty.java
@@ -5,7 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties("algamoney")
 public class AlgamoneyApiProperty {
 	
-	private String origemPermitida = "http://localhost:8080";
+	private String origemPermitida = "http://localhost:8000";
 	
 	private final Seguranca seguranca = new Seguranca();
 	
